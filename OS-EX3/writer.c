5f8b1796d99979cd85b3335bc0740a9d068c82d4
Amir Shavit <amir.shavit@autodesk.com>
2018-12-24 09:56:11 +0200

    hw3 - q1 - add writer

---
+++ OS-EX3/writer.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>    // execvp(), pipe()
+#include "Commons.h"
+
+int main(int argc, const char * argv[]) {
+
+  if (argc != 2) {
+		fprintf(stderr, "Wrong number of Arguments: Missing prefix\n\n");
+		exit(MISSING_ARGS);
+	}
+
+  int bytes;
+  char c;
+  const char* pre = argv[1];
+  printf("%s", pre);
+
+  while ((bytes = read(0, &c, sizeof(c))) > 0) {
+    printf("%c", c);
+    if (c == '\n')
+      printf("%s", pre);
+  }
+
+  if (bytes == -1) {
+      printf("MAIN: Error reading from pipe");
+      exit(PIPE_READ_ERROR);
+  }
+
+  // No need for anymore pipes
+  close(0);
+  close(1);
+  return 0;
+}
